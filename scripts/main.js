d7c7415a5891e3fa3d75149fd0af75b56c74f3f0
Evgeniy Kondratyev <evgeniy.kondratyev@danfoss.com>
2019-03-29 14:21:27 +0300

    select and counting nutrition feature added

--- scripts/main.js
+++ scripts/main.js
@@ -2,27 +2,29 @@ window.addEventListener("DOMContentLoaded",function(){
     
     //The main product constructor
     
-    function Product(n,p,c,f){
+    function Product(n,p,c,f,k){
         this.amount=100;
         this.name=n;
         this.prots=p;
         this.carbs=c;
         this.fats=f;
+        this.kkal=k;
         this.count=function(gramm){
             var gramm=gramm/100;
             this.nprots=this.prots*gramm;
             this.ncarbs=this.carbs*gramm;
             this.nfats=this.fats*gramm;
+            this.nkkal=this.kkal*gramm;
         }
     }
     
     //The main array with all products. Insert here new products
     
     let products=[
-        new Product("none","0","0","0"),
-        new Product("rice","0","78.9","0.7"),
-        new Product("backweat","0","54.4","3.2"),
-        new Product("egg","12.7","0.7","10.9")
+        new Product("none","0","0","0", "300"),
+        new Product("rice","0","78.9","0.7", "300"),
+        new Product("backweat","0","54.4","3.2", "300"),
+        new Product("egg","12.7","0.7","10.9", "300")
     ]
     
     //The main 
@@ -32,7 +34,8 @@ window.addEventListener("DOMContentLoaded",function(){
         let constructor=document.getElementById(id),
             addBtn=document.querySelector(".add-btn"),
             mealItme=document.querySelector(".meal-item"),
-            control=document.querySelector(".control");
+            control=document.querySelector(".control"),
+            clonedEl=mealItme.cloneNode(true);
 
         function removeMealItem(){
             let bttns=document.querySelectorAll(".remove-btn");
@@ -46,25 +49,34 @@ window.addEventListener("DOMContentLoaded",function(){
             })
         }
 
-        function addNewMealItem(){
-            let removeBtn=document.createElement("div");
-            removeBtn.innerHTML="-";
-            removeBtn.classList.add("remove-btn");
-            let newEl=mealItme.cloneNode(true);
-            newEl.append(removeBtn);
-            constructor.insertBefore(newEl, control);
-            removeMealItem();
-            initProductSelect();
-        }
         
-        function fillInputs(products, secetvalue, elements){
+        
+        function fillInputs(products, selectvalue, elements){
+            
             for(let i=0; i<products.length;i++){
-                if(products[i]["name"]==secetvalue){
+                if(products[i]["name"]==selectvalue){
+                    let product=products[i],prots,carbs,fats,kkal,amount;
                     elements.forEach(function(item){
                         if(item.className!==undefined && item.className.indexOf("field")>=0){
+                            
+                            if(item.children[0].name=="prots"){prots=item.children[0]; console.log(prots)};
+                            if(item.children[0].name=="carbs"){carbs=item.children[0]};
+                            if(item.children[0].name=="fats"){fats=item.children[0]};
+                            if(item.children[0].name=="kkal"){kkal=item.children[0]};
+                            if(item.children[0].name=="amount"){amount=item.children[0]};
+                             
                             item.children[0].value=products[i][item.children[0].name];
+                            
                         }
                     });
+                    amount.onchange=function(){
+                        
+                        product.count(amount.value);
+                        prots.value=product.nprots.toFixed(2);
+                        carbs.value=product.ncarbs.toFixed(2);
+                        fats.value=product.nfats.toFixed(2);
+                        kkal.value=product.nkkal.toFixed(2);
+                    }
                 }
             }
         }
@@ -78,6 +90,17 @@ window.addEventListener("DOMContentLoaded",function(){
                     fillInputs(products, selectValue, elements);
                 };
             });
+        }
+        
+        function addNewMealItem(){
+            let removeBtn=document.createElement("div");
+            removeBtn.innerHTML="-";
+            removeBtn.classList.add("remove-btn");
+            let newEl=clonedEl.cloneNode(true);
+            newEl.append(removeBtn);
+            constructor.insertBefore(newEl, control);
+            removeMealItem();
+            initProductSelect();
         }
         
         addBtn.addEventListener("click", addNewMealItem);
